c636cdd02b3478aea54abbb21ee305afee4ec82f
thanapon <thanapon_kung_@hotmail.com>
2014-11-06 11:03:06 +0700

    First Commit

---
+++ HotCoffee/RegisterCoffee.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HotCoffee
+{
+    public partial class RegisterCoffee : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
